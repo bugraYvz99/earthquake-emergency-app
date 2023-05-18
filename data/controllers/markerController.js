04d623e2fc3ce2d58c60f704be3d9344fe0fe91a
bugraYvz99 <you@example.com>
2023-05-18 04:41:02 +0300

    new rateMarker function

--- data/controllers/markerController.js
+++ data/controllers/markerController.js
@@ -19,3 +19,28 @@ exports.createMarker = async (req, res) => {
       .json({ success: false, message: "Marker could not be created" });
   }
 };
+
+// Marker puanlama endpoint'i
+exports.rateMarker = async (req, res) =>  {
+  try {
+   
+    const markerId = req.params.id; // Puanlanacak marker'ın _id bilgisi
+
+    const marker = await Marker.findById(markerId);
+
+    if (!marker) {
+      return res.status(404).json({ message: "Marker bulunamadı." });
+    }
+
+    // Puanlama işlemi
+    marker.puan = req.body.puan;
+
+    await marker.save();
+
+    res.status(200).json({ success: true, message: "Marker puanlandı." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Marker puanlama işlemi başarısız oldu." });
+  }
+}
+
