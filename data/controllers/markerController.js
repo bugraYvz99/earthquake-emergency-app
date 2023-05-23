007994aedd4c111855533d9ed12b05caaea9b1a2
bugraYvz99 <bugrayavuz@live.com>
2023-05-24 00:56:56 +0300

    changes for incident model

--- data/controllers/markerController.js
+++ data/controllers/markerController.js
@@ -1,57 +1,50 @@
-const markerService = require("../services/markerService")
+const markerService = require("../services/markerService");
 
 exports.createMarker = async (req, res) => {
   try {
-    const { position, binaAdi, hasarMiktari, address, rate } = req.body
+    const { position, address } = req.body;
 
-    marker = markerService.createMarker(
-      position,
-      binaAdi,
-      hasarMiktari,
-      address,
-      rate
-    )
-
-    res.status(200).json({ success: true, marker })
+    marker = await markerService.createMarker(position, address);
+    res.status(200).json({ success: true, marker });
   } catch (err) {
-    console.error(err)
+    console.error(err);
     res
       .status(500)
-      .json({ success: false, message: "Marker could not be created" })
+      .json({ success: false, message: "Marker could not be created" });
   }
-}
+};
 
 // Marker puanlama endpoint'i
 exports.rateMarker = async (req, res) => {
   try {
-    const markerId = req.params.id // Puanlanacak marker'ın _id bilgisi
+    const markerId = req.params.id; // Puanlanacak marker'ın _id bilgisi
 
-    const marker = await Marker.findById(markerId)
+    const marker = await Marker.findById(markerId);
 
     if (!marker) {
-      return res.status(404).json({ message: "Marker bulunamadı." })
+      return res.status(404).json({ message: "Marker bulunamadı." });
     }
 
     // Puanlama işlemi
-    marker.puan = req.body.puan
+    marker.puan = req.body.puan;
 
-    await marker.save()
+    await marker.save();
 
-    res.status(200).json({ success: true, message: "Marker puanlandı." })
+    res.status(200).json({ success: true, message: "Marker puanlandı." });
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).json({
       success: false,
-      message: "Marker puanlama işlemi başarısız oldu."
-    })
+      message: "Marker puanlama işlemi başarısız oldu.",
+    });
   }
-}
+};
 exports.getAllMarkers = async (req, res) => {
   try {
-    const markers = await markerService.getAllMarkers()
-    res.status(200).json({ success: true, markers })
+    const markers = await markerService.getAllMarkers();
+    res.status(200).json({ success: true, markers });
   } catch (err) {
-    console.error(err)
-    res.status(500).json({ success: false, message: "Failed to get markers" })
+    console.error(err);
+    res.status(500).json({ success: false, message: "Failed to get markers" });
   }
-}
+};
--- data/data/models/markerModel.js
+++ data/data/models/markerModel.js
@@ -1,16 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const markerSchema = new mongoose.Schema({
   position: {
     lat: { type: Number },
-    lng: { type: Number }
+    lng: { type: Number },
   },
-  binaAdi: { type: String },
-  hasarMiktari: { type: String, required: false, default: "" },
   address: { type: String },
-  rate: { type: Number }
-})
+  rate: { type: Number, default: 0 },
+});
 
-const Marker = mongoose.model("Marker", markerSchema)
+const Marker = mongoose.model("Marker", markerSchema);
 
-module.exports = Marker
+module.exports = Marker;
