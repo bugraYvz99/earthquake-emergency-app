d49423c270846d3f2897c0148f3acae30f87ff82
bugraYvz99 <you@example.com>
2023-05-25 18:34:52 +0300

    Want location info from user , now user's locaiton is center of Map

--- UI/src/Pages/LogEvent.jsx
+++ UI/src/Pages/LogEvent.jsx
@@ -1,10 +1,32 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Map from "../components/Map"
 
 export const LogEvent = () => {
+  const [location, setLocation] = useState(null)
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords
+          setLocation({ latitude, longitude })
+        },
+        (error) => {
+          console.error("Konum alınamadı:", error)
+        }
+      )
+    } else {
+      console.error("Tarayıcınız konum özelliğini desteklemiyor.")
+    }
+  }, [])
+
   return (
     <div className="flex items-center justify-center">
-      <Map />
+      {location ? (
+        <Map location={location} />
+      ) : (
+        <p>Konum bilgisi alınıyor...</p>
+      )}
     </div>
   )
 }
