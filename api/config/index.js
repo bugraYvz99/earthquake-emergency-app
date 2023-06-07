8d0bf662367c9cb43ecc0d23bc05bbd819dfad65
bugraYvz99 <you@example.com>
2023-06-08 01:27:09 +0300

    config fix

--- api/config/index.js
+++ api/config/index.js
@@ -1,9 +1,9 @@
 require("dotenv").config()
 
 const config = {
-  JWT_SECRET: "secret" || process.env.JWT_SECRET,
-  PORT: 3000 || process.env.PORT,
-  MONGO_URL: "mongodb://mongodb0.example.com:27017" || process.env.MONGO_URL
+  JWT_SECRET: process.env.JWT_SECRET || "secret",
+  PORT: process.env.PORT || 3000,
+  MONGO_URL: process.env.MONGO_URL || "mongodb://mongodb0.example.com:27017"
 }
 
 module.exports = config
